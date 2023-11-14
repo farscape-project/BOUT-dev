07db9f9190847eb856bbecc1133587fe810cc59c
Jony Castagna <jony.castagna@stfc.ac.uk>
2023-11-14 15:48:56 +0000

    Paired with StylES commit 759a32eddf9f7582cf6d50de239c2b4023884f46: Paper final version

--- examples/hasegawa-wakatani/hw.cxx
+++ examples/hasegawa-wakatani/hw.cxx
@@ -11,8 +11,11 @@
 #include <bout/smoothing.hxx>
 
 
+#include <chrono>
+using namespace std::chrono;
 
-void initPythonModule(PyObject **pModule, PyObject **pInitFlow, PyObject **pFindLESTerms) {
+
+void initPythonModule(PyObject **pModule, PyObject **pInitFlow, PyObject **pFindLESTerms, PyObject **pWritePoissonDNS) {
 
 
   // Initialize Python interpreter
@@ -51,6 +54,17 @@ void initPythonModule(PyObject **pModule, PyObject **pInitFlow, PyObject **pFind
     fprintf(stderr, "Python function not found!\n");
   }
 
+
+  // Load Python functions and execute it
+  *pWritePoissonDNS = PyObject_GetAttrString(*pModule, "writePoissonDNS");
+
+  if (!(*pWritePoissonDNS) || !PyCallable_Check(*pWritePoissonDNS)) {
+    Py_DECREF(pModule);
+    PyErr_Print();
+    fprintf(stderr, "Python function not found!\n");
+  }
+
+
   return;
 
 }
@@ -59,11 +73,12 @@ void initPythonModule(PyObject **pModule, PyObject **pInitFlow, PyObject **pFind
 
 
 
-void closePythonModule(PyObject **pModule, PyObject **pInitFlow, PyObject **pFindLESTerms) {
+void closePythonModule(PyObject **pModule, PyObject **pInitFlow, PyObject **pFindLESTerms, PyObject **pWritePoissonDNS) {
 
   // shutdown Python interpreter
   Py_DECREF(pInitFlow);
   Py_DECREF(pFindLESTerms);
+  Py_DECREF(pWritePoissonDNS);
   Py_DECREF(pModule);
 
   if (Py_FinalizeEx() < 0) {
@@ -177,7 +192,7 @@ double* initFlow(double dx, double dy, Field3D n, Field3D phi, Field3D vort, PyO
 
 
 
-double* findLESTerms(int pStep, int pStepStart, double dx, double simtime, Field3D n, Field3D phi, Field3D vort,
+double* findLESTerms(int pStep, int pStepStart, double dx, double simtime, Field3D n, Field3D phi, Field3D vort, Field3D pPhiVort, Field3D pPhiN,
   PyObject *pModule, PyObject *pFindLESTerms) {
 
   // local variables
@@ -187,7 +202,7 @@ double* findLESTerms(int pStep, int pStepStart, double dx, double simtime, Field
   PyArrayObject *pReturn = NULL;  
 
   const int SIZE  = n.getNz();
-  const int SIZE2 = 4+3*SIZE*SIZE;
+  const int SIZE2 = 4+5*SIZE*SIZE;
   const int ND    = 1;
 
   int i;
@@ -220,6 +235,8 @@ double* findLESTerms(int pStep, int pStepStart, double dx, double simtime, Field
         pLES[cont + 0*N_LES*N_LES] = n(i,j,k);
         pLES[cont + 1*N_LES*N_LES] = phi(i,j,k);
         pLES[cont + 2*N_LES*N_LES] = vort(i,j,k);
+        pLES[cont + 3*N_LES*N_LES] = pPhiVort(i,j,k);
+        pLES[cont + 4*N_LES*N_LES] = pPhiN(i,j,k);                
         cont = cont+1;
       }
 
@@ -278,6 +295,116 @@ double* findLESTerms(int pStep, int pStepStart, double dx, double simtime, Field
 
 
 
+
+
+
+
+double* writePoissonDNS(int pStep, int pStepStart, double dx, double simtime, Field3D n, Field3D phi, Field3D vort, Field3D pPhiVort, Field3D pPhiN,
+  PyObject *pModule, PyObject *pWritePoissonDNS) {
+
+  // local variables
+  PyObject *pValue = NULL;
+  PyObject *pArray = NULL;  
+  PyArrayObject *pArgs = NULL;
+  PyArrayObject *pReturn = NULL;  
+
+  const int SIZE  = n.getNz();
+  const int SIZE2 = 4+5*SIZE*SIZE;
+  const int ND    = 1;
+
+  int i;
+  int j;
+  int k;
+  int cont;
+
+  double* fLES;
+  double* pLES = new double[SIZE2];
+
+  if (!pLES) {
+      fprintf(stderr, "Out of memory when allocating array pLES!\n");
+  }
+
+  npy_intp dims[1]{SIZE2};
+
+
+
+  // pass n and vort arrays to pLES
+  pLES[0] = double(pStep);
+  pLES[1] = double(pStepStart);
+  pLES[2] = dx;
+  pLES[3] = simtime;
+
+  cont=4;
+  int N_LES = n.getNz();
+  for(int i=2; i<n.getNx()-2; i++)   // we assume 2 guards cells in x-direction
+    for(int j=0; j<1; j++)
+      for(int k=0; k<n.getNz(); k++){
+        pLES[cont + 0*N_LES*N_LES] = n(i,j,k);
+        pLES[cont + 1*N_LES*N_LES] = phi(i,j,k);
+        pLES[cont + 2*N_LES*N_LES] = vort(i,j,k);
+        pLES[cont + 3*N_LES*N_LES] = pPhiVort(i,j,k);
+        pLES[cont + 4*N_LES*N_LES] = pPhiN(i,j,k);                
+        cont = cont+1;
+      }
+
+
+  // convert to numpy array   
+  pArray = PyArray_SimpleNewFromData(ND, dims, NPY_DOUBLE, reinterpret_cast<void*>(pLES));
+  if (pArray)
+  {
+
+    // create arguments
+    pArgs = reinterpret_cast<PyArrayObject*>(pArray);
+    if (pArgs!=NULL) {
+
+      // call function
+      pValue = PyObject_CallFunctionObjArgs(pWritePoissonDNS, pArray, NULL);
+
+      if (pValue!=NULL) {
+
+        pReturn = reinterpret_cast<PyArrayObject*>(pValue);
+        //printf("Dimensions of returned n array are: %d\n", PyArray_NDIM(pReturn));
+
+        // convert result back to C++
+        fLES = reinterpret_cast<double*>(PyArray_DATA(pReturn));
+
+        // decrement Python object counter
+        // Py_DECREF(pReturn);
+        // Py_DECREF(pValue);
+        // Py_DECREF(pArgs);
+        // Py_DECREF(pArray);
+
+      } else {
+        Py_DECREF(pValue);
+        Py_DECREF(pArgs);
+        Py_DECREF(pWritePoissonDNS);
+        Py_DECREF(pModule);
+        PyErr_Print();
+        fprintf(stderr, "Call to Python function failed!\n");
+      }
+    
+    } else {
+      PyErr_Print();
+      fprintf(stderr, "Arguments not created!\n");
+    }
+
+  } else {
+    PyErr_Print();
+    fprintf(stderr, "Array not created!\n");
+  }
+
+
+  delete [] pLES;
+
+  return fLES;
+
+}
+
+
+
+
+
+
 class HW : public PhysicsModel {
 private:
   Field3D n, vort; // Evolving density and vorticity
@@ -287,9 +414,10 @@ private:
   PyObject *pModule;
   PyObject *pInitFlow;
   PyObject *pFindLESTerms;
+  PyObject *pWritePoissonDNS;
 
   int pStep      = 0;
-  int pStepStart = 200;
+  int pStepStart = 200;  // the first call to StylES occurs when step will be 1!
 
   double deltax;
   double deltaz;
@@ -300,6 +428,13 @@ private:
   Field3D pPhiVort;
   Field3D pPhiN;
 
+  
+
+  int totCount = 0;
+  std::chrono::time_point<std::chrono::high_resolution_clock> timeStart;
+  std::chrono::time_point<std::chrono::high_resolution_clock> timeStop;
+  std::chrono::microseconds totDuration1{0};
+  std::chrono::microseconds totDuration2{0};
 
   // Model parameters
   BoutReal alpha;     // Adiabaticity (~conductivity)
@@ -351,7 +486,7 @@ protected:
 
 
     if (pStep==0) {
-      initPythonModule(&pModule, &pInitFlow, &pFindLESTerms);
+      initPythonModule(&pModule, &pInitFlow, &pFindLESTerms, &pWritePoissonDNS);
     }
 
     CELL_LOC outloc = n.getLocation();
@@ -377,7 +512,7 @@ protected:
         }
 
     // // close Python console
-    // closePythonModule(&pModule, &pInitFlow, &pFindLESTerms);
+    // closePythonModule(&pModule, &pInitFlow, &pFindLESTerms, &pWritePoissonDNS);
 
 
     // Communicate variables
@@ -437,14 +572,21 @@ protected:
       psimtime = time;
     }
 
-
+    // pPhiVort = bracket(phi, vort, bm);
+    // pPhiN    = bracket(phi, n, bm);
+    // double simtime2 = time;
+    // rLES = writePoissonDNS(pStep, pStepStart, deltax, simtime2, n, phi, vort, pPhiVort, pPhiN, pModule, pWritePoissonDNS);
 
     if (pStep>=pStepStart)
     {
-      if (psimtime!=time){
+      if (psimtime<time){
+
+        pPhiVort = bracket(phi, vort, bm);
+        pPhiN    = bracket(phi, n, bm);
+
         double simtime = time;
         output_progress.print("\r");
-        rLES = findLESTerms(pStep, pStepStart, deltax, simtime, n, phi, vort, pModule, pFindLESTerms);
+        rLES = findLESTerms(pStep, pStepStart, deltax, simtime, n, phi, vort, pPhiVort, pPhiN, pModule, pFindLESTerms);
         int N_LES = n.getNz();
         int LES_it = int(rLES[0]);
         int cont=1;
@@ -475,19 +617,39 @@ protected:
 
         }
 
-       psimtime = time;
+        psimtime = time;
+
       }
 
     } else {
 
+      if (pStep>pStepStart)
+      {
+        timeStart = high_resolution_clock::now();
+      }
+
       pPhiVort = bracket(phi, vort, bm);
       pPhiN    = bracket(phi, n, bm);
+
+      if (pStep>pStepStart)
+      {
+        timeStop = high_resolution_clock::now();
+        auto duration1 = duration_cast<microseconds>(timeStop - timeStart);
+        totDuration1 = totDuration1 + duration1;
+        // printf("timing convective bracket %lld\n", totDuration1.count());
+      }
 
     }
     
 
 
     // Modified H-W equations, with zonal component subtracted from resistive coupling term
+    if (pStep>pStepStart)
+    {
+      timeStart = high_resolution_clock::now();
+    }
+
+
     Field3D nonzonal_n = n;
     Field3D nonzonal_phi = phi;
     if (modified) {
@@ -499,6 +661,14 @@ protected:
     ddt(vort) = -pPhiVort + alpha*(nonzonal_phi - nonzonal_n);
     ddt(n)    = -pPhiN    + alpha*(nonzonal_phi - nonzonal_n) - kappa*DDZ(phi);
 
+    if (pStep>pStepStart)
+    {
+      timeStop = high_resolution_clock::now();
+      auto duration1 = duration_cast<microseconds>(timeStop - timeStart);
+      totDuration1 = totDuration1 + duration1;
+      // printf("timing convective all %lld\n", totDuration2.count());
+    }
+
     return 0;
   }
   
@@ -506,14 +676,31 @@ protected:
 
 
   int diffusive(BoutReal UNUSED(time)) {
+
+    if (pStep>pStepStart)
+    {
+      timeStart = high_resolution_clock::now();
+    }
+
     // Diffusive terms
     mesh->communicate(n, vort);
 
-    ddt(vort) = + Dvort*Delp2(vort);
-    ddt(n)    = + Dn*Delp2(n);
+    ddt(vort) = - Dvort*Delp4(vort);
+    ddt(n)    = - Dn*Delp4(n);
+
+
+    if (pStep>pStepStart)
+    {
+      timeStop = high_resolution_clock::now();
+      auto duration2 = duration_cast<microseconds>(timeStop - timeStart);
+      totDuration2 = totDuration2 + duration2;
+      totCount=totCount+1;
+      // printf("timing diffusion %lld  %d\n", totDuration2.count(), totCount);
+    }
 
     return 0;
   }
+
 };
 
 
